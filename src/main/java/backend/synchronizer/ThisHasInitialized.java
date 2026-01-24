09e761c2e8d3275f9062afb658ee18bf003ba0db
programmerCC <18770248413@163.com>
2026-01-24 18:41:33 +0800

    feat(synchronizer): 移除班级同步器和相关工具类
    - 删除了 ClassSynchronizer 类及其功能
    - 移除了 SynchronizeToolKit 对象及其初始化和启动方法
    - 删除了 UsbDriverDetector 工具类及跨平台U盘检测功能
    - 清理了相关的协程作用域和通道通信机制
    - 移除了对 Windows 系统的 U 盘驱动器检测支持
    - 删除了 USB 驱动器信息数据类和系统调用实现
    - 删除了 ThisHasInitialized 异常

--- src/main/java/backend/synchronizer/ThisHasInitialized.java
+++
@@ -1,7 +0,0 @@
-package backend.synchronizer;
-
-public class ThisHasInitialized extends RuntimeException {
-    public ThisHasInitialized() {
-        super("这个类已经被初始化过了");
-    }
-}
--- src/main/kotlin/backend/synchronizer/ClassSynchronizer.kt
+++
@@ -1,8 +0,0 @@
-package tech.cpstudio.backend.synchronizer
-
-/**
- * 班级信息同步工具，绑定到班级，用于同步某班级的分值数据并与其他iScore实例互通
- * @author cc-cpstudio
- */
-class ClassSynchronizer {
-}
